eb79851b776064c93c626263abd5224bb5bfdf8f
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-02-26 17:25:13 -0300

    Feat: btnClose

---
+++ src/Components/Btns/BtnClose/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+import { IoMdClose } from "react-icons/io";
+
+export default function BtnClose () {
+  return(
+    <div className="btn-close ">
+      <IoMdClose/>
+    </div>
+  )
+}
