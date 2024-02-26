db1001c16d4c3cd905d9b2b66df26fe9eb6757b4
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-02-26 17:25:48 -0300

    Feat: NavMobile

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
 import { VscAccount } from "react-icons/vsc";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { GoGear } from "react-icons/go";
 import { IoIosMenu } from "react-icons/io";
+import MenuAcount from '../MenuAccount';
+import BtnClose from '../Btns/BtnClose';
 
 const Header = () => {
+  const [navAcoount, setNavAccount] = useState(false)
+  const [navMenu, setMenu] = useState(true)
+  function account () {
+    setNavAccount(true)
+  }
+  function close () {
+    setNavAccount(false)
+  }
+  function openMenu () {
+    setMenu(false)
+  }
+  function closeMenu () {
+    setMenu(true)
+  }
+
   return (
     <header className="header">
       <div className="logo-and-account-container">
@@ -15,23 +32,37 @@ const Header = () => {
               <h1 className="logo">Movie Net</h1>
           </div>
         </Link>
-        <div className="menu-container">
+        <div className={navMenu ? "menu-container" : "menu-container-mobile" }>
           <nav className="nav">
-            <Link to="/" className="link">Início</Link>
-            <Link to="/filmes" className="link">Filmes</Link>
-            <Link to="/sobre" className="link">Sobre</Link>
+            <div onClick={closeMenu} className='close-menu'>
+              <BtnClose/>
+            </div>
+            <div>
+              <Link  onClick={closeMenu}  to="/" className="link">Início</Link>
+            </div>
+            <div>
+              <Link  onClick={closeMenu}  to="/filmes" className="link">Filmes</Link>
+            </div>
+            <div>
+              <Link  onClick={closeMenu}  to="/sobre" className="link">Sobre</Link>
+            </div>
           </nav>
         </div>
-        
+  
         <div className="user-menu">
-          <Link to="/conta" className="menu-link"><VscAccount/></Link>
+          <div onClick={account} className={navAcoount ? "menu-link" : "menu-mobile"}><VscAccount/></div>
           <Link to="/notificacoes" className="menu-link"><IoIosNotificationsOutline/></Link>
           <Link to="/configuracoes" className="menu-link"><GoGear/></Link>
           <div>
-          <IoIosMenu/>
+          <div onClick={openMenu} className='is-menu'>
+            <IoIosMenu/>
+          </div>
         </div>
+        {navAcoount ? <MenuAcount onClickClose={close}/> : <>  </> }
         </div>
       </div>
+      
+      
     </header>
   );
 };
